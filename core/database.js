308bf9d881991508b503ab5c8fbf829314b147ef
gechangning123 <7767747+gechangning123@user.noreply.gitee.com>
2026-04-26 21:37:47 +0800

    feat: 完成

--- core/database.js
+++ core/database.js
@@ -1,6 +1,5 @@
 export const dbConnect = () => console.log('DB Connected');
 
 export const saveUser = (user) => {
-  console.log('saveUser', user);发个人推广xxxx
-  return new Promise((resolve, reject) => {
+  console.log('saveUser', user);
 };
