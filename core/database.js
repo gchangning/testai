974bf5d49be42d911af3ea661e49ad99a7bea925
gechangning123 <7767747+gechangning123@user.noreply.gitee.com>
2026-04-26 22:57:45 +0800

    feat: 完成uuuuuu

--- core/database.js
+++ core/database.js
@@ -3,7 +3,7 @@ export const dbConnect = () => console.log('DB Connected');
 export const DB_DESC = '随便写一句话都行';
 export const DB_DESC2 = '再随便写几个字';
 export const DB_DESC3 = '继续随便写一句';
-export const DB_NOTE = '今天就先随便写到这里。';
+export const DB_NOTE = '今天就先随便写到这里hh。';
 
 export const saveUser = (user) => {
   console.log('saveUser', user);
